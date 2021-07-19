e6df0d7f05d1de8af61333a112bc882b89af886f
Matias <matiasalarcon55@hotmail.com>
2021-07-19 13:57:36 -0300

    Install mysql2

--- bd.js
+++ bd.js
@@ -1,13 +1,14 @@
-const mysql=require("mysql")
-const util=require("util")
+const mysql = require('mysql2');
+const util = require('util');
+require('dotenv').config();
 
-const pool=mysql.createPool({
-    host:"remotemysql.com",
-    user:"3tTwQnZmxy",
-    password:"N0ZaBhCo2t",
-    database:"3tTwQnZmxy"
-})
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
-pool.query=util.promisify(pool.query)
+pool.query = util.promisify(pool.query);
 
-module.exports=pool
+module.exports = pool;
